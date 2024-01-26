6f4487991951643a430613ebd5b825fb2557b996
Jim Driscoll <jim.a.driscoll@gmail.com>
2024-01-26 08:16:59 +0000

    Partial repair: most graphs now appear

--- renderContextCanvas.js
+++ renderContextCanvas.js
@@ -26,9 +26,10 @@ class RenderContextCanvas extends RenderContext {
         if (!canvas) {
             const document = this.#container.ownerDocument
             canvas = document.createElement("canvas")
-            canvas.width = 1000
+            canvas.width = 2000
             canvas.height = 1000
             canvas.style.width = "1000px"
+            canvas.style.height = "500px"
             this.#container.append(canvas)
             this.#canvas = canvas
         }
@@ -50,8 +51,10 @@ class RenderContextCanvas extends RenderContext {
         if(!context) {
             throw new Error("No context")
         }
-        context.translate(x, y)
-        context.scale(2000/w, 500 / h) // TODO
+        context.scale(2000/w, 2000 / w) // TODO
+        context.translate(-x, -y)
+
+        context.save()
 
         context.beginPath()
         context.moveTo(x + w / 10, y)
@@ -75,6 +78,7 @@ class RenderContextCanvas extends RenderContext {
         context.fillText("" + topRight.y, x + w - metrics.width, y + h)
 
         context.translate(w / 10 + x / 10, y / 10)
+        context.restore()
         context.scale(9 / 10, 9 / 10)
         return this
     }
@@ -123,14 +127,13 @@ class RenderContextCanvas extends RenderContext {
         if(!context) {
             throw new Error("No context")
         }
-        context.fillRect(0, 0, 1000, 1000)
+        context.resetTransform()
+        context.fillStyle = "rgb(240,240,240)"
+        context.fillRect(0, 0, 2000, 1000)
     }
 
     setViewBox(box) {
         const [x, y, w, h] = box.split(/ /).map(v => +v) // TODO
-
-        this.canvas.width = 2000
-        this.canvas.height = 2000
     }
 }
 
