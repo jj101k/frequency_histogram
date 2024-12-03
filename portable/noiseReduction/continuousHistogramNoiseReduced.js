8e2fa3b271f02a915d0eba51652f12887e8c050a
Jim Driscoll <jim.a.driscoll@gmail.com>
2024-12-03 14:14:06 -0600

    Noise reduction fix: it's supposed to be the next 10 values, not including current

--- portable/noiseReduction/continuousHistogramNoiseReduced.js
+++ portable/noiseReduction/continuousHistogramNoiseReduced.js
@@ -63,7 +63,8 @@ class ContinuousHistogramNoiseReduced extends ContinuousHistogram {
                 } else {
                     // f1: It's >20% of the mean of the next 10 pending
                     // values
-                    const n10mean = orderedFrequenciesReal.slice(i + 1, i + 1 + 10).reduce(
+                    const offset = i + 1
+                    const n10mean = orderedFrequenciesReal.slice(offset + 1, offset + 1 + 10).reduce(
                         (p, c) => ({t: p.t + scaler.displayY(c), c: p.c + 1}), {t: 0, c: 0})
                     if(n10mean.c && 5 * vScaled < n10mean.t / n10mean.c) {
                         lastScaled = vScaled
